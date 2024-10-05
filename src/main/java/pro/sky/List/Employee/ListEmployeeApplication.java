23c6e3fd7697dd5aab32151918e87a97f763d219
etretjakova <etretjakowa@yandex.ru>
2024-10-05 16:47:07 +0300

    initial commit

---
+++ src/main/java/pro/sky/List/Employee/ListEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.List.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListEmployeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/List/Employee/ListEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.List.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
